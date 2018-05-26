cd358e7156b47f5f395d8029f5933cee40acb3df
shinji <rill0131@gmail.com>
2018-05-26 20:04:02 +0900

    動画撮影、url取得まで

--- spajam2018/ViewController.swift
+++ spajam2018/ViewController.swift
@@ -7,17 +7,56 @@
 //
 
 import UIKit
+import SwiftyCam
 
-class ViewController: UIViewController {
-
+class ViewController: SwiftyCamViewController, SwiftyCamViewControllerDelegate {
+    
+    var movieFlag = false
+    
+    // caputureボタン
+    @IBOutlet var captureButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // SwiftyCamのdelegate
+        cameraDelegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func startCaptureMovie(_ sender: Any) {
+        if(movieFlag == false) {
+            startVideoRecording()
+            movieFlag = true
+            captureButton.setTitle("撮ってる", for: .normal)
+        }else {
+            stopVideoRecording()
+            movieFlag = false
+            captureButton.setTitle("動画を取る", for: .normal)
+        }
+    }
+    
+    // 動画撮影開始
+    // startVideoRecording()で、ビデオ撮影を開始した時に、このメソッドが呼ばれます
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didBeginRecordingVideo camera: SwiftyCamViewController.CameraSelection) {
+        
+    }
+    
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didFinishProcessVideoAt url: URL) {
+        // ビデオ撮影を終了し、ビデオの処理が終わった時に、このメソッドが呼ばれます
+        // 撮影データは、（url: URL）で受け取ることができます
+        print(url)
+    }
+    
+    // 動画撮影終わり
+    // stopVideoRecording()で、ビデオ撮影を終了した時に、このメソッドが呼ばれます
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didFinishRecordingVideo camera: SwiftyCamViewController.CameraSelection) {
+        // apiに送る
+        print("ビデオが撮影されました")
+        
     }
 
 
