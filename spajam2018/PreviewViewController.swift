e3989d14daaa11079c0d970d12c03521d3d52ffb
shinji <rill0131@gmail.com>
2018-05-27 04:32:56 +0900

    update

--- spajam2018/PreviewViewController.swift
+++ spajam2018/PreviewViewController.swift
@@ -10,15 +10,34 @@ import UIKit
 import AVKit
 import AVFoundation
 import Alamofire // Alamofireをimport
+import SwiftGifOrigin
 
 class PreviewViewController: UIViewController {
 
     var url: URL?
     
+    @IBOutlet var loadingImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // loadingを表示
+        
+        
+//        // loadingを表示
+          let loadingGif = UIImage.gif(name: "loading")
+          loadingImg.image = loadingGif
+          loadingImg.loadGif(name: "loading")
+//        // UIImageView 初期化
+//        let imageView = UIImageView(image:loadingGif!)
+//        // 画面の横幅を取得
+//        let screenWidth:CGFloat = view.frame.size.width
+//        let screenHeight:CGFloat = view.frame.size.height
+//
+//        // 画像の中心を画面の中心に設定
+//        imageView.center = CGPoint(x:screenWidth/2, y:screenHeight/2)
+//
+//        // UIImageViewのインスタンスをビューに追加
+//        self.view.addSubview(imageView)
         
         // apiに投げる
         
@@ -30,15 +49,15 @@ class PreviewViewController: UIViewController {
         
         
         
-        let path = url!
-        let videoPlayer = AVPlayer(url: path)
-        
-        // 動画プレイヤーの用意
-        let playerController = AVPlayerViewController()
-        playerController.player = videoPlayer
-        self.present(playerController, animated: true, completion: {
-            videoPlayer.play()
-        })
+//        let path = url!
+//        let videoPlayer = AVPlayer(url: path)
+//
+//        // 動画プレイヤーの用意
+//        let playerController = AVPlayerViewController()
+//        playerController.player = videoPlayer
+//        self.present(playerController, animated: true, completion: {
+//            videoPlayer.play()
+//        })
     }
     
     override func didReceiveMemoryWarning() {
